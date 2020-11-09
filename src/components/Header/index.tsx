25f75e7da8938a24833628a00dc626c1313fe973
Marcelo da Silva Ataíde <contato.marceloataide@gmail.com>
2020-11-09 18:15:21 -0300

    teste2[fix] com styled-components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,10 @@
 import * as S from './styles'
 
 const Header = () => (
-<<<<<<< HEAD
   <S.Wrapper>
     <h1>Header</h1>
   </S.Wrapper>
-=======
-  <h1>Header</h1>
->>>>>>> 61c9bb514e1dea92ebe44d1b65a5bd300893682b
+
 )
 
 export default Header
